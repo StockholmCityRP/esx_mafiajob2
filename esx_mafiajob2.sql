01eaf097f342fa6dc3186d2def632599696f2a15
Kasper <35449118+KasperE1@users.noreply.github.com>
2018-02-20 11:41:22 +0100

    Update and rename esx_mafiajob.sql to esx_mafiajob2.sql

--- esx_mafiajob2.sql
+++ esx_mafiajob2.sql
@@ -11,7 +11,7 @@ INSERT INTO `addon_inventory` (name, label, shared) VALUES
 ;
 
 INSERT INTO `jobs` (`name`, `label`, `whitelisted`) VALUES
-('mafia2', '💸 Mafia2', 1);
+('mafia2', 'Nuevo Paraíso', 1);
 
 
 INSERT INTO `job_grades` (`job_name`, `grade`, `name`, `label`, `salary`, `skin_male`, `skin_female`) VALUES
