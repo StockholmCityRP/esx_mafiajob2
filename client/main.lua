d8625bf3d6da180433bdbed3cd86ed7d65658856
Kasper <35449118+KasperE1@users.noreply.github.com>
2018-02-23 14:47:59 +0100

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -100,7 +100,7 @@ function OpenArmoryMenu(station)
       {label = _U('get_weapon'), value = 'get_weapon'},
       {label = _U('put_weapon'), value = 'put_weapon'},
       {label = _U('get_stock'),  value = 'get_stock'},
-      {label = _U('deposit_stock')  value = 'put_stock'},
+      {label = _U('deposit_stock'),  value = 'put_stock'},
     }
 
     if PlayerData.job.grade_name == 'boss' then
