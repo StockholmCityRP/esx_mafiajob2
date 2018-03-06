fdd2b57b31eca8dd5b62dcbf57b3979849ad3e16
Kasper <35449118+KasperE1@users.noreply.github.com>
2018-03-06 19:55:24 +0100

    Changed name

--- esx_mafiajob2.sql
+++ esx_mafiajob2.sql
@@ -11,7 +11,7 @@ INSERT INTO `addon_inventory` (name, label, shared) VALUES
 ;
 
 INSERT INTO `jobs` (`name`, `label`, `whitelisted`) VALUES
-('mafia2', 'Nuevo Paraíso', 1);
+('mafia2', 'Familjen', 1);
 
 
 INSERT INTO `job_grades` (`job_name`, `grade`, `name`, `label`, `salary`, `skin_male`, `skin_female`) VALUES
